697ad3b15bd418a1cfaa9a8253cfc42852b20038
Bhoobalan B R <44297926+bhoobalan-bhoo@users.noreply.github.com>
2024-06-19 16:29:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const http = require('http');
 const requestIp = require('request-ip');
+const url = require('url');
+
+// Function to parse query parameters
+const parseQueryParams = (queryString) => {
+    const params = {};
+    new URLSearchParams(queryString).forEach((value, key) => {
+        params[key] = value;
+    });
+    return params;
+};
 
 const server = http.createServer((req, res) => {
     // Get user agent
@@ -8,17 +18,47 @@ const server = http.createServer((req, res) => {
     // Get IP address
     const clientIp = requestIp.getClientIp(req);
 
+    // Parse URL and query parameters
+    const parsedUrl = url.parse(req.url, true);
+    const queryParams = parsedUrl.query;
+
     // Log user agent and IP address
     console.log('User Agent:', userAgent);
     console.log('IP Address:', clientIp);
     
-    console.log('Request Body:', req.body);
-    console.log('Request Query:', req.query);
-    console.log('Request Params:', req.params);
-    
-    // Respond to the client
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!\n');
+    // Log query parameters
+    console.log('Request Query:', queryParams);
+
+    // Initialize request body
+    let body = '';
+
+    // Collect the request body data
+    req.on('data', chunk => {
+        body += chunk.toString();
+    });
+
+    // End event when the entire body has been received
+    req.on('end', () => {
+        // Parse body if content-type is JSON
+        if (req.headers['content-type'] === 'application/json') {
+            try {
+                body = JSON.parse(body);
+            } catch (e) {
+                console.error('Invalid JSON body:', e);
+            }
+        }
+
+        // Log request body
+        console.log('Request Body:', body);
+
+        // Log path parameters (example parsing)
+        const pathParams = parsedUrl.pathname.split('/').filter(Boolean); // Example: ['path', 'to', 'resource']
+        console.log('Request Params:', pathParams);
+
+        // Respond to the client
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Hello World!\n');
+    });
 });
 
 const PORT = 3000;
